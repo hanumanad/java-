4cd2f35e3adbee0075e41a146251a67ad825ae8c
hanumanad <hanuman.scm4u@gmail.com>
2018-05-14 03:36:56 -0700

    modified

--- change.sh
+++ change.sh
@@ -1,13 +1,2 @@
-#hj
-# changing directory
-#! /bin/bash
-cd /opt/chintu
-touch chintu.txt chiru.txt
-if [ "$?" = "0" ];
-then
-rm -f chiru.txt
-else
-echo "Cannot change directory!" 1>&2
-exit 1
-fi
+hi
 
